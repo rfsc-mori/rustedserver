1e6ae960ede573c61136269f77e839b079129316
Rafael Fillipe Silva <silva.rafaelfillipe@gmail.com>
2020-05-29 22:43:10 -0300

    Create config.lua on first run.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::fs;
+use std::path::Path;
 use tokio::task;
 
 #[tokio::main]
 async fn main() {
     task::spawn(async {
         println!("Rustedserver (Temporary) - Version 0.1.0.");
+
+        if !Path::new("./config.lua").exists() {
+            fs::copy("./config.lua.dist", "./config.lua")
+                .expect("Failed to create config.lua");
+        }
     });
 }
