977b396b0034d24f82c97a117db683a73f9c2be7
Rafael Fillipe Silva <silva.rafaelfillipe@gmail.com>
2020-05-29 22:08:07 -0300

    Use Tokio for tasks.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    task::spawn(async {
+        println!("Rustedserver (Temporary) - Version 0.1.0.");
+    });
 }
